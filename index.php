8f2611539e245da91cd2353842b1826b0d650b42
kalin5850 <kalin5850@gmail.com>
2016-05-21 00:49:10 -0400

    added a test file

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Page B</title>
+    <link href="main.css" type="text/css" rel="stylesheet" >
+  </head>
+  <body>
+     <h1> Murach logo </h1>
+     <img src="image/murachlogo.JPG" width="100" height="100">
+     <p> Here it is again: </p>
+     <img src="image/murachlogo.JPG" width="100" height="100">
+     <aside>
+          <h1> Just an aside </h1>
+     </aside>
+  </body>
+</html>
